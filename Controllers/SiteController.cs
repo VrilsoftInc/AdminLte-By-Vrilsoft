d8e26dbc03286f674f0ec9713cd91254269324bc
Vrilsoft <social@vrilsoft.com>
2020-10-06 19:45:52 +0100

    Removed routes from the controller since they are set in the RouteConfig.

--- Controllers/SiteController.cs
+++ Controllers/SiteController.cs
@@ -8,30 +8,25 @@ namespace AdminLte_By_Vrilsoft.Controllers
 {
     public class SiteController : Controller
     {
-        [Route("About")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
             return View();
         }
-        [Route("Contact")]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
             return View();
         }
-        [Route("Team")]
         public ActionResult Team()
         {
             return View();
         }
-        [Route("Email-us")]
         public ActionResult EmailUs()
         {
             return View();
         }
-        [Route("Privacy")]
         public ActionResult Privacy()
         {
             return View();
