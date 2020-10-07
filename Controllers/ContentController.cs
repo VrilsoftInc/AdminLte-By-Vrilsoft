85af51a5c73a7710ac019686d7a5a0b69774f9f7
VrilsoftInc <68389929+VrilsoftInc@users.noreply.github.com>
2020-10-07 14:37:16 +0100

    Update ContentController.cs
    Removed routing attribute since routing is managed by the routing config

--- Controllers/ContentController.cs
+++ Controllers/ContentController.cs
@@ -5,8 +5,6 @@ namespace AdminLte_By_Vrilsoft.Controllers
     [HandleError]
     public class ContentController : Controller
     {
-        [Route("Index")]
-        [Route("/")]
         public ActionResult Index()
         {
             return View();
